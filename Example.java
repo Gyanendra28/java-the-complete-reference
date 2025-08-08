851ce0577594288757474f88102e0b45a391032c
Gyanendra <2003gyanendra@gmail.com>
2025-08-08 14:58:53 +0530

    Initial commit

---
+++ Example.java
@@ -0,0 +1,15 @@
+/*
+    This is a simple Java program.
+    Call this file "Example.java".
+*/
+
+public class Example { // Main class
+    // Your program begins with a call to main().
+    public static void main(String[] args) { // Main method
+        System.out.println("This is a simple Java program.");
+    }
+}
+
+// To compile: C:\>javac Example.java (where javac is the compiler and Example.java is the source code)
+// The javac compiler creates a file called Example.class (which is the bytecode version of the program)
+// To run: C:\java Example (where java is the Java Application Launcher)
